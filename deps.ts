17b10dc136015a371b6969a7d2f373da6d73ee53
Alexander Zenov <alexander.zenov@gmail.com>
2020-12-23 13:51:00 +0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,6 @@
 export { ensureDir } from "https://deno.land/std/fs/mod.ts";
 export { bold } from "https://deno.land/std/fmt/colors.ts";
-export { Confirm, Input } from "https://deno.land/x/cliffy/prompt/mod.ts";
+export {
+  Confirm,
+  Input,
+} from "https://deno.land/x/cliffy@v0.16.0/prompt/mod.ts";
